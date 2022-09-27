5a3a109dd2e24a2d6b47db4488baba4c91446db8
AlmiranteFuchs <58078331+AlmiranteFuchs@users.noreply.github.com>
2022-09-26 23:48:55 -0300

    feat: phone number treatment
    + baileys only for now, I'm tired af

--- src/api_models/baileys/api_baileys.ts
+++ src/api_models/baileys/api_baileys.ts
@@ -80,8 +80,8 @@ export class baileys_api implements API {
             })
 
             sock.ev.on('messages.upsert', async (message: any) => {
-             /*    if (message?.key?.fromMe) return;
-                await this._send_message_upstream(message); */
+                /*    if (message?.key?.fromMe) return;
+                   await this._send_message_upstream(message); */
             });
             return true;
         } catch (error) {
@@ -100,6 +100,9 @@ export class baileys_api implements API {
 
     async send_message(phone_number: string, text_message: string, reply?: boolean | undefined): Promise<CommForm> {
         try {
+            // Treats phone to Brazil only
+            phone_number = this._treat_phone_number(phone_number);
+            
             await this._bot_client.sendMessage(`${phone_number}@s.whatsapp.net`, { text: text_message });
             return { result: true, message: "Mensagem enviada com sucesso" };
         } catch (error) {
@@ -151,6 +154,29 @@ export class baileys_api implements API {
     // Getters && private methods
     private _get_qr(): string {
         return this._qr_log;
+    }
+
+    private _treat_phone_number(phone_number: string): string {
+        let treated_phone_number: string = phone_number;
+
+        if (treated_phone_number.startsWith("+")) {
+            treated_phone_number = treated_phone_number.replace("+", "");
+        }
+        if (!treated_phone_number.startsWith("55")) {
+            treated_phone_number = `55${treated_phone_number}`;
+        }
+
+        // Remove all non numeric characters
+        treated_phone_number = treated_phone_number.replace(/\D/g, "");
+
+        // Counts to see ifs there's a 9 in the beginning
+        let to_count = treated_phone_number.substring(4, treated_phone_number.length); 
+        if (to_count.length === 9) {
+            //remove the 9
+            treated_phone_number = treated_phone_number.substring(0, 4) + treated_phone_number.substring(5, treated_phone_number.length);
+        }
+
+        return treated_phone_number;
     }
 
     private async _clear_session(): Promise<boolean> {
